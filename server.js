ec2261d282872b78804d2f38c7e73e662ca924e0
Bharat ksagar <surgitechsuppliers@gmail.com>
2025-04-20 00:40:24 +0530

    fix: added server.js and updated scripts for Render deploy

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
+// server.js
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// React च्या build फोल्डरमधून static फाइल्स सर्व्ह कर
 app.use(express.static(path.join(__dirname, "build")));
 
-// unknown रूटसाठी React index.html परत पाठव
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
